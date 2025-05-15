4a17fea84e35896b11577e2ac1151707d75776ee
Nathan Probert <nathanprobert@rogers.com>
2025-05-15 19:15:32 -0400

    Add screen layout

---
+++ .config/i3/screenlayout.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xrandr --output DP-4 --mode 1920x1080 --rate 164.92 --pos 1080x320 --rotate normal --primary \
+       --output HDMI-0 --mode 1920x1080 --rate 74.97 --pos 0x0 --rotate left
+
+feh --bg-fill ~/Downloads/rick_and_morty_outer_space.jpg --bg-fill ~/Downloads/rick_and_morty_portal.jpg
